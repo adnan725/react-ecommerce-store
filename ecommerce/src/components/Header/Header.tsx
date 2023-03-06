46f4ede97e9d877ebd2a352bb6a5ce55ccba0354
Adnan Ali <adnanali6688@gmail.com>
2023-03-06 17:43:22 +0100

    implemented sass variables and mixins

--- ecommerce/src/components/Header/Header.tsx
+++ ecommerce/src/components/Header/Header.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import styles from "./Header.module.scss";
 
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.headerWrapper}>
+        <div className={styles.log}>LOGO</div>
+        <div className={styles.links}>
+          <ul>
+            <li>Catagories</li>
+            <li>Deals</li>
+            <li>What's New</li>
+            <li>Delivery</li>
+          </ul>
+        </div>
+        <div className={styles.search}>
+          <label htmlFor="search">
+            <span>icons</span>
+            <input type="text" id="search" placeholder="Search Product" />
+          </label>
+        </div>
+        <div className={styles.account}>ACCOUNT</div>
+        <div className={styles.cart}>CART</div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
