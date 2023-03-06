7cf04e8b4977bd31aefdaf4f0a79f9348c5341e6
Adnan Ali <adnanali6688@gmail.com>
2023-03-06 18:15:17 +0100

    implemented header component

--- ecommerce/src/components/Header/Header.tsx
+++ ecommerce/src/components/Header/Header.tsx
@@ -6,7 +6,10 @@ const Header = (props: Props) => {
   return (
     <div className={styles.container}>
       <div className={styles.headerWrapper}>
-        <div className={styles.log}>LOGO</div>
+        <div className={styles.logo}>
+          <img src="../../../../public/media/icons/shop.png" alt="logo" />
+          <span>ShopKart</span>
+        </div>
         <div className={styles.links}>
           <ul>
             <li>Catagories</li>
@@ -17,12 +20,23 @@ const Header = (props: Props) => {
         </div>
         <div className={styles.search}>
           <label htmlFor="search">
-            <span>icons</span>
+            <span>
+              <img
+                src="../../../../public/media/icons/search.png"
+                alt="megnifier"
+              />
+            </span>
             <input type="text" id="search" placeholder="Search Product" />
           </label>
         </div>
-        <div className={styles.account}>ACCOUNT</div>
-        <div className={styles.cart}>CART</div>
+        <div className={styles.account}>
+          <img src="../../../../public/media/icons/user.png" alt="user" />
+          <span>Account</span>
+        </div>
+        <div className={styles.cart}>
+          <img src="../../../../public/media/icons/cart.png" alt="user" />
+          <span>Account</span>
+        </div>
       </div>
     </div>
   );
